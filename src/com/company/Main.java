d5ab37e67462bc30fd71cad96b222c54c2172211
13 <baktyguljumagazyeva@gmail.com>
2022-09-20 19:28:05 +0600

    DZ28.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,5 +1,15 @@
+package com.company;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Integer> list = Arrays.asList(54,57,87,89,23,43,12,90);
+        System.out.println("max: ");
+      Integer var= list.stream().max(Integer::compare).get();
+        System.out.println(var);
+        System.out.println("квадрат четных чисел: ");
+        list.stream().filter(n -> n % 2 == 0).map(n->n*n).forEach(System.out::println);
     }
 }
